67a17b1a67b64ff05bdb14c4a387802e9223725c
marstonad20 <marstonad20@gcc.edu>
2024-02-19 15:09:09 -0500

    Added some more dummy prints to Main

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Foo");
         System.out.println("Bar");
+        System.out.println("My hands are typing");
+        System.out.println("words.");
     }
 }
